d9537e0aa75721726bc8df50678ad3381cf95191
Mauricio Machuca <mauricio208@gmail.com>
2019-02-24 22:42:28 -0400

    fix on urls

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -11,6 +11,7 @@ import Contacto from './views/Contacto.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
--- gaidi/urls.py
+++ gaidi/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,9 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('appointments_manager.urls')),
-    path("",
+    re_path(r'^.*$',
         TemplateView.as_view(template_name="application.html"),
         name="app",
     ),
-    # path('api/', include(router.urls))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
